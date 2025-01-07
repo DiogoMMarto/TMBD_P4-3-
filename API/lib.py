5a98ab125dadb10fec7ee074af80eb6ab80d7868
DiogoMMarto <diogo.marto@ua.pt>
2025-01-07 22:50:06 +0000

    ~basic stuff

---
+++ API/lib.py
@@ -0,0 +1,4 @@
+def process_image(image_file):
+    # Your image processing logic here
+    # Return the result as a string
+    return "sus"
---
+++ API/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, File, UploadFile
+
+from lib import process_image
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/")
+async def process(file: UploadFile = File(...)):
+    return {
+        "filename": file.filename,
+        "class" : process_image(file.file)
+        }
