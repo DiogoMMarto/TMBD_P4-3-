ebd2ebacbba3fb4b34dd59b72fe808e1acbef3de
DiogoMMarto <diogo.marto@ua.pt>
2025-01-08 22:57:09 +0000

    API fix

--- API/main.py
+++ API/main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI, File, UploadFile
 
 from lib import process_image
 
-app = FastAPI(prefix="/api")
+app = FastAPI()
 
-@app.get("/")
+@app.get("/api")
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/")
+@app.post("/api")
 async def process(file: UploadFile = File(...)):
     return {
         "filename": file.filename,
