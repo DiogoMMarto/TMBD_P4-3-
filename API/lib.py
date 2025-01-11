5bf59c4d0000931af710a394e34b7522bdb1d53f
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2025-01-11 16:03:33 +0000

    Add model to API and update response data

--- API/lib.py
+++ API/lib.py
@@ -1,4 +1,18 @@
-def process_image(image_file):
-    # Your image processing logic here
-    # Return the result as a string
-    return "sus"
+from PIL import Image
+import numpy as np
+import io
+
+WIDTH = 30
+HEIGHT = 30
+CHANNELS = 3
+
+def process_image(content, model):
+    image = Image.open(io.BytesIO(content))
+    image = image.resize([WIDTH, HEIGHT])
+    image = np.array(image) / 255
+    
+    pred = np.argmax(
+        model.predict(image.reshape(1, WIDTH, HEIGHT, CHANNELS))
+    )
+    
+    return str(pred)
--- API/main.py
+++ API/main.py
@@ -1,16 +1,35 @@
-from fastapi import FastAPI, File, UploadFile
-
+from fastapi import FastAPI, UploadFile
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+import tensorflow as tf
 from lib import process_image
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = tf.keras.models.load_model("model.h5")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/api")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Traffic Sign Classification API"}
+
 
 @app.post("/api")
-async def process(file: UploadFile = File(...)):
+async def process(file: UploadFile):
+    content = await file.read()
+    pred = process_image(content, model)
+
     return {
         "filename": file.filename,
-        "class" : process_image(file.file)
-        }
+        "class": pred,
+        "traffic_sign": f"/static/traffic_signs/{pred}.png",
+    }
